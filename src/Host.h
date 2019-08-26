bb7ec15fba0381feb75050847a04d4a833e76cfc
Mike Conley <sousesider@gmail.com>
2019-08-26 01:18:30 -0400

    Negotiate and store MSSP data in Lua table (#3025)

--- src/Host.h
+++ src/Host.h
@@ -257,6 +257,7 @@ public:
     QFont mCommandLineFont;
     QString mCommandSeparator;
     bool mEnableGMCP;
+    bool mEnableMSSP;
     bool mEnableMSDP;
     bool mServerMXPenabled;
     QTextStream mErrorLogStream;
